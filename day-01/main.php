a5c9af82e416249327452209647aa5e52ba22e6b
mnobrecastro <mnobrecastro@gmail.com>
2023-12-01 21:39:26 +0100

    Solution day-01.

--- day-01/main.php
+++ day-01/main.php
@@ -1,13 +1,22 @@
 <?php
-function getNumber($str, $end)
-{
+
+function genLine(string $filename) : Generator {
+    $f = fopen($filename, 'r');
+    while(($line = fgetcsv($f)) !== false){
+        yield $line;
+    }
+    fclose($f);
+    return;
+}
+
+function getNumber(string $str, bool $end) : int {
     $number = array("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
     for($i=0; $i < 10; $i++) {
         if($end) {
             if(str_ends_with($str, $number[$i])){
                 return $i;
             }
-        }else {
+        } else {
             if(str_starts_with($str, $number[$i])){
                 return $i;
             }
@@ -16,49 +25,67 @@ function getNumber($str, $end)
     return -1;
 }
 
-$sum = 0;
-$f = fopen("input.txt", 'r');
-while(($line = fgetcsv($f)) !== false){
-    $s = implode($line);
-    $a = ''; $b = '';
-    $lbuf = ''; $rbuf = '';
-    $i = 0; $j = strlen($s)-1;
-    while($i <= $j){
-        if($a == ''){
-            $val = ord($s[$i]) - ord('0');
-            if(0 <= $val && $val <= 9){
-                $a = $val;
-            } else {
-                $lbuf .= $s[$i];
-                $val = getNumber($lbuf, true);
-                if ($val >= 0) {
+function calibrate($filename) : int {
+    $sum = 0;
+    $generator = genLine($filename);
+    foreach($generator as $line) {
+        $s = implode($line);
+        $a = ''; $b = '';
+        $lbuf = ''; $rbuf = '';
+        $i = 0; $j = strlen($s) - 1;
+        while($i <= $j){
+            if($a == '') {
+                $val = ord($s[$i]) - ord('0');
+                if(0 <= $val && $val <= 9){
                     $a = $val;
                 } else {
-                    $i++;
+                    $lbuf .= $s[$i];
+                    $val = getNumber($lbuf, true);
+                    if ($val >= 0) {
+                        $a = $val;
+                    } else {
+                        $i++;
+                    }
                 }
             }
-        }
-        if($b == '') {
-            $val = ord($s[$j]) - ord('0');
-            if(0 <= $val && $val <= 9){
-                $b = $val;
-            } else {
-                $rbuf = $s[$j] . $rbuf;
-                $val = getNumber($rbuf, false);
-                if ($val >= 0) {
+            if($b == '') {
+                $val = ord($s[$j]) - ord('0');
+                if(0 <= $val && $val <= 9){
                     $b = $val;
                 } else {
-                    $j--;
+                    $rbuf = $s[$j] . $rbuf;
+                    $val = getNumber($rbuf, false);
+                    if ($val >= 0) {
+                        $b = $val;
+                    } else {
+                        $j--;
+                    }
                 }
             }
+            if($a !== '' && $b !== '')
+                break;
         }
-        if($a !== '' && $b !== '') break;
+        if($a == '') $a = $b;
+        if($b == '') $b = $a;
+        $sum += $a*10 + $b;
+        echo "$s -> $a$b\n";
     }
-    if($a == '') $a = $b;
-    if($b == '') $b = $a;
-    $sum += $a*10 + $b;
-    echo "$s -> $a$b\n";
+    return $sum;
 }
-echo $sum;
-fclose($f);
+
+
+
+function main(): void
+{
+    $filename = "sample.txt";
+    $sum = calibrate($filename);
+    if (281 != $sum)
+        return;
+    $filename= "input.txt";
+    $sum = calibrate($filename);
+    echo $sum;
+    return;
+}
+main();
+
 ?>
