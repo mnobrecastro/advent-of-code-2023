a9975f7ce5145b2c27c66d06ad2384d28e8326d2
mnobrecastro <mnobrecastro@gmail.com>
2023-12-01 20:24:48 +0100

    Solved 01-2.

--- day-01/main.php
+++ day-01/main.php
@@ -1,9 +1,27 @@
 <?php
+function getNumber($str, $end)
+{
+    $number = array("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+    for($i=0; $i < 10; $i++) {
+        if($end) {
+            if(str_ends_with($str, $number[$i])){
+                return $i;
+            }
+        }else {
+            if(str_starts_with($str, $number[$i])){
+                return $i;
+            }
+        }
+    }
+    return -1;
+}
+
 $sum = 0;
 $f = fopen("input.txt", 'r');
 while(($line = fgetcsv($f)) !== false){
     $s = implode($line);
     $a = ''; $b = '';
+    $lbuf = ''; $rbuf = '';
     $i = 0; $j = strlen($s)-1;
     while($i <= $j){
         if($a == ''){
@@ -11,15 +29,27 @@ while(($line = fgetcsv($f)) !== false){
             if(0 <= $val && $val <= 9){
                 $a = $val;
             } else {
-                $i++;
+                $lbuf .= $s[$i];
+                $val = getNumber($lbuf, true);
+                if ($val >= 0) {
+                    $a = $val;
+                } else {
+                    $i++;
+                }
             }
         }
         if($b == '') {
             $val = ord($s[$j]) - ord('0');
             if(0 <= $val && $val <= 9){
                 $b = $val;
-            }else{
-                $j--;
+            } else {
+                $rbuf = $s[$j] . $rbuf;
+                $val = getNumber($rbuf, false);
+                if ($val >= 0) {
+                    $b = $val;
+                } else {
+                    $j--;
+                }
             }
         }
         if($a !== '' && $b !== '') break;
