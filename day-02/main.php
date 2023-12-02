2a36cf5076407c0c32f8d7b3aae82290695b1c8a
mnobrecastro <mnobrecastro@gmail.com>
2023-12-02 12:25:19 +0100

    Solved 02-2.

--- day-02/main.php
+++ day-02/main.php
@@ -9,8 +9,11 @@ function gen_line(string $filename) : Generator {
     return;
 }
 
-function parse_line($str) {
-    $cubes = array("red" => 0, "green" => 0, "blue" => 0);
+function parse_line(string $str, bool $find_max=true) {
+    if ($find_max)
+        $cubes = array("red" => 0, "green" => 0, "blue" => 0);
+    else
+        $cubes = array("red" => 1e6, "green" => 1e6, "blue" => 1e6);
     $str_arr = explode(':', $str);
     $game = intval(explode(' ', $str_arr[0])[1]);
     //echo "'$str'", "\n";
@@ -23,7 +26,10 @@ function parse_line($str) {
         foreach($sub_arr as $subset) {
             $subset = substr($subset, 1); // Drop the leading space
             $sub_arr = explode(' ', $subset);
-            $cubes[$sub_arr[1]] = max($cubes[$sub_arr[1]], intval($sub_arr[0]));
+            if ($find_max)
+                $cubes[$sub_arr[1]] = max($cubes[$sub_arr[1]], intval($sub_arr[0]));
+            else
+                $cubes[$sub_arr[1]] = min($cubes[$sub_arr[1]], intval($sub_arr[0]));
         }
         echo $cubes["red"] ." ". $cubes["green"] ." ". $cubes["blue"], "\n";
     }
@@ -34,7 +40,7 @@ function solve($filename, array $target) : int {
     $sum = 0;
     $generator = gen_line($filename);
     foreach($generator as $line) {
-        [$game, $cubes] = parse_line($line);
+        [$game, $cubes] = parse_line($line, false);
         echo "Game $game: " . $cubes["red"] ." ". $cubes["green"] ." ". $cubes["blue"], "\n";
         $flag = false;
         foreach(["red", "green", "blue"] as $color) {
@@ -49,18 +55,40 @@ function solve($filename, array $target) : int {
     return $sum;
 }
 
+function solve2($filename) : int {
+    $sum = 0;
+    $generator = gen_line($filename);
+    foreach($generator as $line) {
+        [$game, $cubes] = parse_line($line);
+        echo "Game $game: " . $cubes["red"] ." ". $cubes["green"] ." ". $cubes["blue"], "\n";
+        $power = 1;
+        foreach(["red", "green", "blue"] as $color) {
+            $power *= $cubes[$color];
+        }
+        $sum += $power;
+    }
+    return $sum;
+}
+
 
 function main(): void
 {
-    $filename = "sample.txt";
+    /**** PART 1 ****/
     $bag = array("red" => 12, "green" => 13, "blue" => 14);
-    $sum = solve($filename, $bag);
-    echo $sum;
-    if (8 !== $sum)
-        return;
-    $filename= "input.txt";
-    $sum = solve($filename, $bag);
-    echo $sum;
+    $res = solve("sample.txt", $bag);
+    echo $res;
+    if (8 !== $res)
+        return;    
+    $res = solve("input.txt", $bag);
+    echo $res;
+    
+    /**** PART 2 ****/
+    $res = solve2("sample.txt");
+    echo $res;
+    if (2286 !== $res)
+        return;    
+    $res = solve2("input.txt");
+    echo $res;
     return;
 }
 main();
