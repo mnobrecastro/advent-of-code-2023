c031f136e65f92168b76e0a8e4c314611b097ab9
mnobrecastro <mnobrecastro@gmail.com>
2023-12-06 00:36:40 +0100

    Solution day-05.

--- day-05/main.php
+++ day-05/main.php
@@ -69,45 +69,6 @@ function solve(string $filename) : int
     return min($seeds);
 }
 
-function expand_seeds($arr)
-{
-    $new = array();
-    for($i = 0; $i < count($arr); $i+=2){
-        for($j = $arr[$i]; $j < $arr[$i] + $arr[$i+1]; $j++)
-            array_push($new, $j);
-    }
-    return $new;
-}
-
-/*function solve2(string $filename) : int
-{
-    $generator = gen_line($filename);
-    // Retrieve header (first line)
-    [$flag, $seeds] = parse_line($generator->current(), $is_header=true);
-    $seeds = expand_seeds($seeds);
-    print_arr($seeds);
-    $generator->next();
-    //Read the rest of the file
-    while ($generator->valid()) {
-        [$flag, $map] = parse_line($generator->current());        
-        if($flag) {
-            $change = array_fill(0, count($seeds), true);
-        }
-        if ($map){          
-            for($j = 0; $j < count($seeds); $j++) {
-                if($change[$j] && $map[1] <= $seeds[$j] && $seeds[$j] <= $map[1] + $map[2] - 1){
-                    $seeds[$j] += $map[0] - $map[1];
-                    $change[$j] = false;
-                }
-            }
-            print_arr($seeds);
-        }
-        $generator->next();
-    }
-    return min($seeds);
-}*/
-
-
 function gen_expand_seeds($arr) : generator
 {
     for($i = 0; $i < count($arr); $i+=2){
@@ -146,9 +107,6 @@ function echo_arr($arr)
 function solve2(string $filename) : int
 {
     [$seeds, $mapping] = parse_file($filename);
-    // foreach($mapping as $maps) {
-    //     echo count($maps), "\n";
-    // }
     $seeds = gen_expand_seeds($seeds);
     $min = -log(0); // INF
     $i = 0;
@@ -178,15 +136,14 @@ function solve2(string $filename) : int
 function main(): void
 {
     /**** PART 1 ****/
-    // $res = solve("sample.txt");
-    // echo $res, "\n";
-    // if (35 !== $res)
-    //     return;    
-    // $res = solve("input.txt");
-    // echo $res, "\n";
+    $res = solve("sample.txt");
+    echo $res, "\n";
+    if (35 !== $res)
+        return;    
+    $res = solve("input.txt");
+    echo $res, "\n";
 
     /**** PART 2 ****/
-    //ini_set('memory_limit', '32G'); //-1
     $res = solve2("sample.txt");
     echo $res, "\n";
     if (46 !== $res)
