12a2ed1ec73a28bf234ed4567ed15cf668d468b2
mnobrecastro <mnobrecastro@gmail.com>
2023-12-20 16:20:11 +0100

    Solved 18-2.

--- day-18/main.php
+++ day-18/main.php
@@ -10,7 +10,7 @@ function gen_line(string $filename) : generator
     return;
 }
 
-function parse_file($filename) : array
+function parse_file(string $filename, bool $is_hexa) : array
 {   
     $arr = array();
     $generator = gen_line($filename);    
@@ -19,6 +19,27 @@ function parse_file($filename) : array
         [$cmd, $hash] = explode("(", $line);
         [$move, $steps] = explode(" ", $cmd);
         $hash = substr($hash, 1, -1);
+        //echo $move." ".$steps." ".$hash."\n";
+        if($is_hexa) {
+            // The last hexadecimal digit encodes the direction to dig:
+            // 0 means R, 1 means D, 2 means L, and 3 means U.
+            switch(hexdec(substr($hash, -1))) {
+                case 0:
+                    $move = "R";
+                    break;
+                case 1:
+                    $move = "D";
+                    break;
+                case 2:
+                    $move = "L";
+                    break;
+                case 3:
+                    $move = "U";
+                    break;
+            }
+            $steps = hexdec(substr($hash, 0, -1));
+        }
+        //echo "* ".$move." ".$steps." ".$hash."\n";
         array_push($arr, array($move, $steps, $hash));
         $generator->next();
     }
@@ -41,60 +62,109 @@ class DirectedGraph
         $this->_inside = array();
     }
 
-    function from_file(string $filename) : string
+    function from_file(string $filename, bool $is_hexa) : void
     {
         // Build the graph
-        $arr = parse_file($filename);
+        $arr = parse_file($filename, $is_hexa);
         $i = 0; $j = 0;
-        $start = sprintf("%d,%d", $i, $j);
-        foreach($arr as [$move, $steps, $hash]){
-            for($k = 0; $k < intval($steps); $k++){
-                $tail = sprintf("%d,%d", $i, $j);
-                switch($move) {
-                    case "U":
-                        $i--;
-                        break;
-                    case "D":
-                        $i++;
-                        break;
-                    case "L":
-                        $j--;
-                        break;
-                    case "R":
-                        $j++;
-                        break;
-                }                
-                $head = sprintf("%d,%d", $i, $j);
-                $this->add_edge($tail, $head);              
-            }            
+        $tail = sprintf("%d,%d", $i, $j);
+        foreach($arr as [$move, $steps, $hash]){ 
+            switch($move) {
+                case "U":
+                    $i -= intval($steps);
+                    break;
+                case "D":
+                    $i += intval($steps);
+                    break;
+                case "L":
+                    $j -= intval($steps);
+                    break;
+                case "R":
+                    $j += intval($steps);
+                    break;
+            }
+            $head = sprintf("%d,%d", $i, $j);
+            //echo $tail." -> ".$head."\n";
+            $this->add_edge($tail, $head);
+            $tail = $head;
         }
-        return $start;
+        return;
     }
 
     public function add_edge($tail, $head) : void
     {
-        if(!in_array($tail, array_keys($this->_adj)))
-            $this->_adj[$tail] = array();
-        array_push($this->_adj[$tail], $head);
+        // if(!in_array($tail, array_keys($this->_adj)))
+        //     $this->_adj[$tail] = array();
+        // array_push($this->_adj[$tail], $head);
+        $this->_adj[$tail] = $head;
     }
     
     public function get_size() : int
     {
-        return count($this->_adj);
+        $sum = 0;
+        foreach($this->_adj as $tail => $head) {
+            //echo "^".$tail." -> ".$head."\n";
+            [$t_i, $t_j] = explode(",", $tail);
+            $t_i = intval($t_i);
+            $t_j = intval($t_j);
+            [$h_i, $h_j] = explode(",", $head);
+            $h_i = intval($h_i);
+            $h_j = intval($h_j);
+            $sum += abs($t_i - $h_i) + abs($t_j - $h_j);
+        }
+        return $sum;
+        // return count($this->_adj);
     }
 
     public function flood_fill($vertex) : void
     {
-        if(in_array($vertex, array_keys($this->_adj))) {
-            return;
-        } else if (in_array($vertex, $this->_inside)) {
+        [$i, $j] = explode(",", $vertex);
+        $i = intval($i);
+        $j = intval($j);
+
+        foreach($this->_adj as $tail => $head) {
+            //echo "*".$tail." -> ".$head."\n";
+            [$t_i, $t_j] = explode(",", $tail);
+            $t_i = intval($t_i);
+            $t_j = intval($t_j);
+            [$h_i, $h_j] = explode(",", $head);
+            $h_i = intval($h_i);
+            $h_j = intval($h_j);
+            if($t_i == $h_i && $t_i == $i){
+                // Horizontal
+                if($t_j > $h_j){
+                    // Left
+                    if($h_j <= $j && $j <= $t_j) {
+                        return;
+                    }
+                } else if ($t_j < $h_j){
+                    // Right
+                    if($t_j <= $j && $j <= $h_j) {
+                        return;
+                    }
+                }
+            }else if($t_j == $h_j && $t_j == $j){
+                // Vertical
+                if($t_i > $h_i){
+                    // Up
+                    if($h_i <= $i && $i <= $t_i) {
+                        return;
+                    }
+                } else if ($t_i < $h_i){
+                    // Down
+                    if($t_i <= $i && $i <= $h_i) {
+                        return;
+                    }
+                }
+            }
+        }
+        if (in_array($vertex, $this->_inside)) {
             return;
         } else {
             array_push($this->_inside, $vertex);
-
-            [$i, $j] = explode(",", $vertex);
-            $i = intval($i);
-            $j = intval($j);
+            if(count($this->_inside) % 10000 == 0){
+                echo "Inside: ".count($this->_inside)."\n";
+            }
             // Up
             $this->flood_fill(sprintf("%s,%s", $i-1, $j));
             // Down
@@ -112,17 +182,53 @@ class DirectedGraph
         return count($this->_inside);
     }
 
+    public function get_area() : int
+    {
+        $area = 0.0;
+        $start = array(0.0, 0.0);
+        $flag = true;
+        foreach($this->_adj as $tail => $head) {            
+            
+            [$t_i, $t_j] = explode(",", $tail);
+            $t_i = floatval($t_i);
+            $t_j = floatval($t_j);
+            [$h_i, $h_j] = explode(",", $head);
+            $h_i = floatval($h_i);
+            $h_j = floatval($h_j);
+
+            if($flag) {
+                $flag = false;
+                continue;
+            }
+
+            $area += $this->_signed_area($start, array($t_i, $t_j), array($h_i, $h_j));
+            //echo "area: ".$area."\n";
+        }
+        return $area;
+    }
+
     protected $_adj;
     protected $_inside;
+
+    protected function _signed_area(array $p1, array $p2, array $p3) : float
+    {
+        return 0.5 * (
+            -$p1[0] * $p2[1] + $p1[1] * $p2[0] +
+            $p1[0] * $p3[1] - $p2[0] * $p3[1] +
+            -$p1[1] * $p3[0] + $p2[1] * $p3[0] );
+    }
 }
 
-function solve(string $filename) : int
+function solve(string $filename, bool $is_hexa=false) : int
 {        
     $g = new DirectedGraph();
-    $start = $g->from_file($filename);
+    $start = $g->from_file($filename, $is_hexa);
     $contour = $g->get_size();
-    $g->flood_fill("1,1");
-    $fill = $g->get_inside();
+    //$g->flood_fill("1,1");
+    //$fill = $g->get_inside();
+    $area = $g->get_area();    
+    $fill = $area + 1 - $contour/2; // Pick's theorem   
+    echo "contour: ".$contour." fill: ".$fill."\n";
     return $contour + $fill;
 }
 
@@ -135,10 +241,21 @@ function main(): void
     if (62 !== $res)
         return;
     printf("... sample passed!\n");
-    $tic = microtime(true); 
+    $tic = microtime(true);
     $res = solve("input.txt");
     $toc = microtime(true);
     printf("Answer 1: %d in %.3f ms.\n", $res, ($toc-$tic)*1e3);
+
+    /**** PART 2 ****/
+    $res = solve("sample.txt", $is_hexa=true);
+    echo $res;
+    if (952408144115 !== $res)
+        return;
+    printf("... sample passed!\n");
+    $tic = microtime(true); 
+    $res = solve("input.txt", $is_hexa=true);
+    $toc = microtime(true);
+    printf("Answer 2: %d in %.3f ms.\n", $res, ($toc-$tic)*1e3);
 }
 main();
 
